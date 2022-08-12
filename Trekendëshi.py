f08652391aa92bdd02bfb1fdb425688d73b3b43e
Blerina Demaj <95138459+blerinademaj@users.noreply.github.com>
2022-08-12 10:22:05 +0200

    Guri Morina

---
+++ Trekendëshi.py
@@ -0,0 +1,24 @@
+import turtle
+
+# Pjesa e kodit me turtle librarine
+trekendeshi = turtle.Turtle()
+trekendeshi.pencolor("red")
+
+for i in range(3):
+   trekendeshi.forward(80)
+   trekendeshi.right(120)
+
+#Pjesa e kodit me metoden print(), ushtrimet në klasë
+#Vizatimi i trekëndëshit duke përdorur metodën print()
+
+print("        /\\")
+print("       /  \\")
+print("      /    \\")
+print("     /      \\")
+print("    /        \\")
+print("   /          \\")
+print("  /            \\")
+print(" /              \\")
+print("/________________\\")
+
+
